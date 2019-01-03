91abf3582e97342f8a18b9f7c248482932db853f
natalie mahmalie <nataliemah67@gmail.com>
2019-01-03 18:15:42 +0200

    final commit

--- index.js
+++ index.js
@@ -1,43 +1,22 @@
-const express = require('express'),
-bodyparser = require('body-parser'),
-func = require('./controller'),
-app = express(),
+const express = require('express');
+
+const ctrl = require('./controller');
+const call=require('./call');
+app = express();
 port = process.env.PORT || 3000;
 
-app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({extended:true}));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+
+app.get('/users',call(ctrl.getAllUsr));
+app.get('/users/:team&:status',call(ctrl.getUsrData))
+app.post('/users/:id',call(ctrl.setStatusByOpenAndClose))
 
 app.all('*',(req,res,next)=>{
-  func.connect();
-  console.log(`logged to database`);
-  next();
-});
-
-app.get('/getAllUsr',(req,res)=>{
-    // res.status(200).json({"allUsers":func.getAllUsr()});
-    res.status(200).send(func.getAllUsr());
-});
-
-app.post('/getUsrData',(req,res)=>{
-    var id = req.body.id;
-  res.status(200).json(func.getUsrData(id));
-});
-
-app.get('/getStatusByOpenAndClose/:open/:close',(req,res)=>{
-  var open = req.params.open,
-  close = req.params.close;
-  res.json(func.getStatusByOpenAndClose(open,close));
-});
-
-app.all('/getStatusByOpenAndClose/',(req,res)=>{
-  var open = req.body.open,
-  close = req.body.close;
-  res.json(func.getStatusByOpenAndClose(open,close));
-});
-
-app.all('*',(req,res)=>{
-  res.sendfile(`${__dirname}/api.html`);
-});
+  res.send("you need to write a specific path")
+  next()
+})
 
 app.listen(port,()=>{
   console.log(`listen on port ${port}`);
--- newUsr.js
+++ newUsr.js
@@ -1,13 +1,15 @@
-var mongoose = require('mongoose'),
-      equipment = require('./equipment');
+const mongoose  = require('mongoose')
+const equipment=require('./equipment')
+const schema = {
+    name: { type: String, required: true},
+    id: { type: String, required: true },
+    status: { type: String, required:true } ,
+    team:  { type: String, required:true } ,
+    equipment:[equipment]
+}
 
-var newUsr = new mongoose.Schema({
-  name:{type:String},
-  id:{type:String},
-  status:{type:String},
-  equipment:[equipment]
-  });
+const post_schema = new mongoose.Schema(schema)
+module.exports = mongoose.model('NewUsr', post_schema, 'users')
 
-var newUsr = mongoose.model('newUsr',newUsr);
 
-module.exports = newUsr;
+
